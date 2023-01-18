3e35cf275a4d24c04027a43ce83f3f457b9b009b
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-01-18 16:14:42 +0100

    Update new styles and utilities classes

--- helpers/formatter.ts
+++ helpers/formatter.ts
@@ -1,7 +1,18 @@
-export function dateFormatted(date: Date | string): string {
-  return new Date(date).toLocaleDateString("en-US", {
-    day: "2-digit",
-    month: "long",
-    year: "numeric",
-  });
+// LOCAL INTERFACES: 
+export interface Props
+{
+  style: string;
+  currency: string;
+  maximumFractionDigits?: number | string | undefined;
+  minimumFractionDigits?: number | string | undefined;
 }
+
+
+export const DATE_FORMATTED = ( date: Date | string = new Date() ) =>
+{
+  return new Date( date ).toLocaleDateString( 'en-US', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  } );
+};
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,20 +1,16 @@
 // import next resources https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { DATE_FORMATTED } from '../../helpers';
 
-// import utilities and helpers
-import { dateFormatted } from '../../helpers';
-
-// Setting Types or Interfaces
-export interface Props {
+// Local Interfaces
+export interface Props
+{
   message: string;
 }
 
-// API Next Handler Backend Functions:
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Props>
-) {
-  res.status(200).json({
-    message: `API is ready and running at: ${dateFormatted(new Date())}`,
-  });
+export default function handler( req: NextApiRequest, res: NextApiResponse<Props> )
+{
+  res.status( 200 ).json( {
+    message: `API is ready and running at: ${DATE_FORMATTED( new Date() )}`,
+  } );
 }
