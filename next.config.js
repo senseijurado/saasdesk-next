6d86ef1a388afb6b75c77b28b50e968802828a9a
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-01-24 20:58:58 +0100

    Modificados next.config | .eslintrc

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   distDir: 'dist',
-  optimizeFonts: true,
-  eslint: {
-    dirs: ['pages/**/*'],
-  },
-  swcMinify: false,
+
 };
 
 module.exports = nextConfig;
